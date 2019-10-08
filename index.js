4ac8163656233d0c7f8a8dce8b9fc8af2bbcfa36
WillHoll <willhollingsworth15@yahoo.com>
2019-10-08 14:05:13 -0600

    complete

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ function addTodo(event) {
     const newItem = document.createElement('li')
     const input = document.querySelector('#item')
     newItem.innerText = input.value
+    newItem.addEventListener('click', completeTodo)
+
+    const deletion = document.createElement('button')
+    deletion.innerText = 'X'
+    deletion.addEventListener('click', removeTodo)
+    newItem.append(deletion)
+    
     const list = document.querySelector('ul')
     list.appendChild(newItem)
+    input.value = ''
+}
+
+function removeTodo(event) {
+    event.target.parentNode.remove()
+}
+
+function completeTodo() {
+    const checked = event.target.getAttribute('aria-checked')
+    if (checked === 'true') {
+        event.target.setAttribute('aria-checked', 'false')
+    } else {
+        event.target.setAttribute('aria-checked', 'true')
+    }
 }
